6e14c445f100aa328c6943a97e57ba34f44bc0c7
ernest-kaminski <kaminski.ern@gmail.com>
2023-10-17 21:54:01 +0200

    Ernest commit 17.10

---
+++ app/src/main/java/com/example/spy/mainFragments/PlayersFragment.java
@@ -0,0 +1,25 @@
+package com.example.spy.mainFragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.spy.R;
+
+public class PlayersFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.players_fragment, container, false);
+
+
+
+        return v;
+    }
+}
---
+++ app/src/main/java/com/example/spy/mainFragments/SpiesFragment.java
@@ -0,0 +1,25 @@
+package com.example.spy.mainFragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.spy.R;
+
+public class SpiesFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.spies_fragment, container, false);
+
+
+
+        return v;
+    }
+}
